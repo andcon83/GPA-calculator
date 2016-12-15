70e0e60d750aa107213b79d4a0762788a13d8bfe
Taylor Want <taylor.want@gmail.com>
2016-12-15 19:27:28 +0000

    final

--- application_controller.rb
+++ application_controller.rb
@@ -14,21 +14,25 @@ class MyApp < Sinatra::Base
   post '/calculate' do
     answer_array=params.values
     new_array=[]
-    answer_array.each do |value|
-      if value.is_a?(String)&&value.length>0||value.is_a?(Integer)
+    answer_array.each_with_index do |value,index|
+      if value=="yes"&&answer_array[index-1]!=""
+        new_array.push(1)
+      elsif value=="no"&&answer_array[index-1]!=""
+        new_array.push(0)
+      elsif value.is_a?(String)&&value.length>0||value.is_a?(Integer)
         new_array.push(value.to_f)
       end
     end
     answer=[]
     new_array.each_with_index do |value,index|
-      if value<1.1
-        answer.push([new_array[index-1],value])
+      if value>1.1
+        answer.push([value, new_array[index+1], new_array[index+2]])
       end
     # puts answer_array
     end
     @unweightedresult=f_gpa(answer)
     @kacunweighted=k_gpa(answer)
-    @weightedresult=@unweightedresult
+    @weightedresult=w_gpa(answer)
     erb :results
   end
 
--- models/model.rb
+++ models/model.rb
@@ -42,11 +42,52 @@ def k_gpa(arr)
   c_total=0
   arr.each do |array|
     if array[0]>70
-      f_total+=4-(0.1*(100-array[0]))
+      f_total+=(4-(0.1*(100-array[0])))*array[1]
       c_total+=array[1]
     else
       c_total+=array[1]
     end
   end
+  f_total/c_total
+end
+
+def w_gpa(arr)
+  
+  f_total=0
+  c_total=0
+  arr.each do |array|
+    if array[0]>96.9
+      f_total+=4.33*array[1]+array[2]
+      c_total+=array[1]
+    elsif array[0]>92.9
+      f_total+=4.0*array[1]+array[2]
+      c_total+=array[1]
+    elsif array[0]>89.9
+      f_total+=3.67*array[1]+array[2]
+      c_total+=array[1]
+    elsif array[0]>86.9
+      f_total+=3.33*array[1]+array[2]
+      c_total+=array[1]
+    elsif array[0]>82.9
+      f_total+=3.0*array[1]+array[2]
+      c_total+=array[1]
+    elsif array[0]>79.9
+      f_total+=2.67*array[1]+array[2]
+      c_total+=array[1]
+    elsif array[0]>76.9
+      f_total+=2.33*array[1]+array[2]
+      c_total+=array[1]
+    elsif array[0]>72.9
+      f_total+=2.0*array[1]+array[2]
+      c_total+=array[1]
+    elsif array[0]>69.9
+      f_total+=1.67*array[1]+array[2]
+      c_total+=array[1]
+    else
+      c_total+=array[1]
+   end
+  end
+  puts f_total
+  puts c_total
   f_total/c_total
 end
